636df385ce4e990c1a12428955c43209fd043fb6
David W.H. Swenson <dwhs@hyperblazer.net>
2015-07-04 19:45:41 +0200

    Cleanup in devtools

--- devtools/ci/after_sucess.sh
+++ devtools/ci/after_sucess.sh
@@ -8,11 +8,11 @@ if [[ "$TRAVIS_BRANCH" != "master" ]]; then
     echo "No deployment on BRANCH='$TRAVIS_BRANCH'"; exit 0
 fi
 
-if [[ "2.7" =~ "$python" ]]; then
-    conda install --yes binstar jinja2
-        conda convert -p all ~/miniconda/conda-bld/linux-64/openpathsampling-dev*.tar.bz2 -o ~/miniconda/conda-bld/
-    binstar -t ${BINSTAR_TOKEN}  upload  --force --u omnia -p openpathsampling-dev $HOME/miniconda/conda-bld/*/openpathsampling-dev*.tar.bz2
-fi
+#if [[ "2.7" =~ "$python" ]]; then
+    #conda install --yes binstar jinja2
+        #conda convert -p all ~/miniconda/conda-bld/linux-64/openpathsampling-dev*.tar.bz2 -o ~/miniconda/conda-bld/
+    #binstar -t ${BINSTAR_TOKEN}  upload  --force --u omnia -p openpathsampling-dev $HOME/miniconda/conda-bld/*/openpathsampling-dev*.tar.bz2
+#fi
 
 if [[ "$python" != "2.7" ]]; then
     echo "No deploy on PYTHON_VERSION=${python}"; exit 0
@@ -21,9 +21,9 @@ fi
 
 # Create the docs and push them to S3
 # -----------------------------------
-conda install --yes pip
-conda config --add channels http://conda.binstar.org/omnia
-conda install --yes `conda build devtools/conda-recipe --output`
+#conda install --yes pip
+#conda config --add channels http://conda.binstar.org/omnia
+#conda install --yes `conda build devtools/conda-recipe --output`
 pip install numpydoc s3cmd
 conda install --yes `cat docs/requirements.txt | xargs`
 
