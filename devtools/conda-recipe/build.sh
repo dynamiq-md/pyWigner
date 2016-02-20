e8a39eaafb1c393ad88eff35671c2e704724b98c
David W.H. Swenson <dwhs@hyperblazer.net>
2016-02-19 21:15:26 -0500

    Try installing dynamiq_engine in build.sh

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+export PYWIGNER=`pwd`
+cd && git clone https://github.com/dynamiq-md/dynamiq_engine
+cd dynamiq_engine && python setup.py install
+cd $PYWIGNER
+
 $PYTHON setup.py install
