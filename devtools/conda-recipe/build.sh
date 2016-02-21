a08918a25f1182ee09fc424d4427b81cde430c28
David W.H. Swenson <dwhs@hyperblazer.net>
2016-02-20 19:14:50 -0500

    Add dynamiq_samplers to build.sh manual installs

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -3,6 +3,8 @@
 export PYWIGNER=`pwd`
 cd && git clone https://github.com/dynamiq-md/dynamiq_engine
 cd dynamiq_engine && python setup.py install
+cd && git clone https://github.com/dynamiq-md/dynamiq_samplers
+cd dynamiq_samplers && python setup.py install
 cd $PYWIGNER
 
 $PYTHON setup.py install
